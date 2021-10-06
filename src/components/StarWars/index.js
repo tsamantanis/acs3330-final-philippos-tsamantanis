f5f77b012b26ca083c1cf62dbe73eaf44563bb20
tsamantanis <philip.tsamantanis@icloud.com>
2021-10-06 16:41:01 -0700

    Minor unstyled input error handling

--- src/components/StarWars/index.js
+++ src/components/StarWars/index.js
@@ -4,11 +4,18 @@ import './starwars.css';
 function StarWars() {
     const [id, setId] = useState(1)
     const [character, setCharacter] = useState(null) 
-    
+    const [error, setError] = useState(null)
+
+    function validate() {
+        return parseInt(id) > 0 && parseInt(id) < 84 && parseInt(id) !== 17
+    }
+
     const makeRequest = () => {
-        fetch('https://swapi.dev/api/people/' + id)
-            .then(res => res.json())
-            .then(data => setCharacter(data.name))
+        if (validate())
+            fetch('https://swapi.dev/api/people/' + id)
+                .then(res => res.json())
+                .then(data => setCharacter(data.name))
+        else setError("Invalid id")
     }
 
   return (
@@ -18,14 +25,15 @@ function StarWars() {
             placeholder="1"
             min="1" 
             max="83"
-            onChange={(e) => setId(e.target.value)}
+            onChange={(e) => {setId(e.target.value); setError(null)}}
         />
         <button
             onClick={makeRequest}
         >
             Search
         </button>
-        { character && <h3>{character}</h3>}
+        { error && <span className="text-error">{error}</span>}
+        { !error && character && <h3>{character}</h3>}
     </div>
   );
 }
